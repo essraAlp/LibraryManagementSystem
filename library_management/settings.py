719d0ccae3bf2652ab5f627062090f251ecd59fc
essraAlp <ealparslan325@gmail.com>
2025-11-19 17:39:58 +0300

    Remove unused import from load_data.py and add django_extensions to installed apps

--- library_management/settings.py
+++ library_management/settings.py
@@ -1,15 +1,10 @@
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
 
-
-
-
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -36,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "user.apps.UserConfig",
     "Books.apps.BooksConfig",
     "Barrow.apps.BarrowConfig",
