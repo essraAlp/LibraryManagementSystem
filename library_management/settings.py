bdaddd2f9fe25e9d93127c103cc9d42964b170ea
essraAlp <ealparslan325@gmail.com>
2025-11-19 17:19:48 +0300

    Update database configuration to use environment variables for user and host settings

--- library_management/settings.py
+++ library_management/settings.py
@@ -1,6 +1,8 @@
 """Django settings for the library_management project."""
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 
 
@@ -76,10 +78,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME", ""),
-        "USER": os.getenv("DB_USER", ""),
+        "USER": os.getenv("DB_USER", "root"),
         "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "3306"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "3006"),
     }
 }
 
